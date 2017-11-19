46edb42e4c0c4b57eaaefee951a09710e0b868cb
naseef012 <naseefmohammed012@gmail.com>
2017-11-20 00:43:20 +0600

    Modified jsp page and management controller so that spring form can be stored in database

--- src/main/java/com/naseef/shoppingbackend/dto/Product.java
+++ src/main/java/com/naseef/shoppingbackend/dto/Product.java
@@ -119,7 +119,6 @@ public class Product
 
     public boolean isActive()
     {
-
         return active;
     }
 
@@ -166,5 +165,24 @@ public class Product
     public void setViews(int views)
     {
         this.views = views;
+    }
+
+    @Override
+    public String toString()
+    {
+        return "Product{" +
+                "id=" + id +
+                ", code='" + code + '\'' +
+                ", name='" + name + '\'' +
+                ", brand='" + brand + '\'' +
+                ", description='" + description + '\'' +
+                ", unitPrice=" + unitPrice +
+                ", quantity=" + quantity +
+                ", active=" + active +
+                ", categoryId=" + categoryId +
+                ", supplierId=" + supplierId +
+                ", purchases=" + purchases +
+                ", views=" + views +
+                '}';
     }
 }
